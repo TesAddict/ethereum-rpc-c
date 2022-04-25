e82fb2887717da34a6d460ce9f63abe7c6376b54
tesaddict <lefty.amperiadis@protonmail.com>
2022-04-24 21:02:51 -0500

    Index from root.

--- tests/test_json.c
+++ tests/test_json.c
@@ -3,15 +3,14 @@
 
 int test_json_add_inline() {
     json_t *root = json_init(OBJECT, NULL, NULL);
-    json_t *param_array = json_init(ARRAY, "params", NULL);
-    json_add_deeper(param_array, json_init(OBJECT, NULL, NULL));
-    json_add_deeper(param_array->deeper, json_init(STRING, "from", "0x0"));
-    json_add_next(param_array->deeper->deeper, json_init(STRING, "to", "0x0"));
-    json_add_next(param_array->deeper->deeper, json_init(STRING, "gas", "0x0"));
-    json_add_next(param_array->deeper->deeper, json_init(STRING, "value", "0x0"));
-    json_add_next(param_array->deeper->deeper, json_init(STRING, "data", "0x0"));
-    json_add_next(param_array, json_init(STRING, "jsonrpc", "2.0"));
-    json_add_deeper(root, param_array);
+    json_add_deeper(root, json_init(ARRAY, "params", NULL));
+    json_add_next(root->deeper, json_init(STRING, "jsonrpc", "2.0"));
+    json_add_deeper(root->deeper, json_init(OBJECT, NULL, NULL));
+    json_add_deeper(root->deeper->deeper, json_init(STRING, "from", "0x0"));
+    json_add_next(root->deeper->deeper->deeper, json_init(STRING, "to", "0x0"));
+    json_add_next(root->deeper->deeper->deeper, json_init(STRING, "gas", "0x0"));
+    json_add_next(root->deeper->deeper->deeper, json_init(STRING, "value", "0x0"));
+    json_add_next(root->deeper->deeper->deeper, json_init(STRING, "data", "0x0"));
     json_to_str(root);
     return 0;
 }
